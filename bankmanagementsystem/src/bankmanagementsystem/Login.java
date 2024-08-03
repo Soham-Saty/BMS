9bd0369537b52d827acf3af251874d69d67b0249
Soham-Satalkar <sohamsatalkar18@gmail.com>
2024-08-03 23:40:02 +0530

    Login class is created

---
+++ bankmanagementsystem/src/bankmanagementsystem/Login.java
@@ -0,0 +1,12 @@
+package bankmanagementsystem;
+
+public class Login {
+
+    Login(){
+        System.out.println("Login class is initialized");
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+
+}
